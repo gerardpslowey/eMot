936cab1bc4513220ecc1c9019f61812fc08a8f03
savagem7 <i0g1n4oj>
2021-02-01 00:50:13 +0000

    working date filter and test file

--- src/browser_history/date_filter.py
+++ src/browser_history/date_filter.py
@@ -4,36 +4,24 @@ from dateutil.relativedelta import relativedelta
 # To use dateutil install the following:
 #   pip install python-dateutil
 
-def main():
-    now = datetime.now()
+class DateFilter:
 
-    print("Today's Date: ", strformat(now))
-    print("Hour: ", hour(now)) 
-    print("Day: ", day(now)) 
-    print("Week: ", week(now)) 
-    print("Month: ", month(now))
-    print("Year: ", year(now))
+    times = ['hour','day','week','month','year']
 
-def hour(time):
-    return strformat(time - timedelta(hours=1))
+    def hour(self):
+        return self.strformat(datetime.now() - timedelta(hours=1))
 
-def day(time):
-    return strformat(time - timedelta(1))
+    def day(self):
+        return self.strformat(datetime.now() - timedelta(1))
 
-def week(time):
-    return strformat(time - timedelta(days=7))
+    def week(self):
+        return self.strformat(datetime.now() - timedelta(days=7))
 
-def month(time):
-    return strformat(time.replace(day=1) + relativedelta(months=+1))
+    def month(self):
+        return self.strformat(datetime.now() + relativedelta(months=+1))
 
-def year(time):
-    return strformat(time + relativedelta(years=-1))
+    def year(self):
+        return self.strformat(datetime.now() + relativedelta(years=-1))
 
-#def allTime(time):
-    # return all the dates
-
-def strformat(time):
-    return time.strftime("%Y-%m-%d %H:%M:%S")   #makes the date a string
-
-if __name__ == '__main__':
-    main()
+    def strformat(self,time):
+        return time.strftime("%Y-%m-%d %H:%M:%S")   #makes the date a string
--- src/browser_history/test.py
+++ src/browser_history/test.py
@@ -3,17 +3,38 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
 
 from browser_history.browsers import Chrome, Firefox, Safari, Edge, Opera, Brave
+from date_filter import DateFilter
 
 def main():
+    print("Time filters include 'hour', 'day', 'week', 'month', or 'year' or '' (all time).")
+    filtr = input('Filter the date: ')
+
+    print("Browser options include 'Chrome', 'Firefox', 'Safari', 'Edge', 'Opera', and 'Brave'.")
     browser = input('Enter the browser: ')
-    f = globals()[browser]()
-    outputs = f.fetch_history()
-    his = outputs.histories
 
-    selected = his[:50]
+    #Check if input not empty and in the possible options.
+    if filtr and filtr in DateFilter.times:
+        date_filter = getattr(DateFilter(), filtr)()
 
-    for element in selected:
-        print(element, end = "\n")
+    #if blank, then use all dates
+    elif not filtr:
+        date_filter = ''
+    else:
+        raise ValueError("Make sure your filter choice is valid and spelled correctly.")
+
+    try:
+        f = globals()[browser]()                    # browser
+        outputs = f.fetch_history()
+        his = outputs.histories
+
+        selected = his[:50]
+
+        for date, url in selected:
+            if date > date_filter:
+                print(f"{date} : {url}", end = "\n")
+    
+    except Exception as e:
+        print(f"{e} : Make sure your browser choice is valid and spelled correctly.")
 
 if __name__ == "__main__":
     main()
