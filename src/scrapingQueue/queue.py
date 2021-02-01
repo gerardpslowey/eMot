c82490596cd00d44d5232f07d73fd9088d2ce488
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2021-02-01 19:16:26 +0000

    Initial attemp at scraping using threads and a job queue

---
+++ src/scrapingQueue/queue.py
@@ -0,0 +1,68 @@
+# Queue of urls 
+from queue import Queue
+
+# Implement threading
+import threading
+import concurrent.futures as futures
+from multiprocessing.pool import ThreadPool
+
+# Resolve import errors
+import os
+import sys
+from pathlib import Path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# Get browser history
+import browser_history
+
+# Scraper
+from urlScraper import scraper
+
+MAX_WORKERS = 10
+
+
+def add_to_queue(urls, q):
+    for url in urls:
+        q.put(url[1])
+
+    print("URLs added to queue")
+    return()
+
+def scrape(site):
+    # while True:
+    #     # gets the url from the queue
+    #     site = queue.get()
+
+    # download the file
+    print("scraping site: " + site)
+    scraper.scrape(site)
+    print(f'task {site} finished') 
+
+    return()
+
+def main():
+    # queue = Queue()
+
+    urls = browser_history.get_history()
+    print("History Retrieved")
+
+    # add_to_queue(urls, queue)
+
+    # set up for threads to work
+    # t = threading.Thread(target=scrape, args=(queue, )) 
+    # t.daemon = True
+    # t.start() 
+    # t.join()
+    # print("Finished scraping!")
+
+    with futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        links = list()
+        for url in urls:
+            links.append(executor.submit(scrape, site=url[1]))
+        
+        results = list()
+        for link in futures.as_completed(links):
+            results.append(link.result())
+
+if __name__ == "__main__":
+    main()
