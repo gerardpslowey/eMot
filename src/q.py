47ba6f46f59950ef98762b2904a9ae99d40b512f
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2021-02-17 20:33:50 +0000

    Started implementing option to blacklist sites which you do not want to scrape

--- src/q.py
+++ src/q.py
@@ -9,34 +9,35 @@ import os, sys
 from pathlib import Path
 from browserHistory.getHistory import GetHistory
 
-# Scraper
+# Scraper and file modification
 from urlProcessor.scraper import Scraper
 from urlProcessor.fileMod import FileMod
+from urlProcessor.urlFilter import filter_blacklisted_url
 
 MAX_WORKERS = 10
 
 def main():
+    blacklist = ['www.facebook.com', 'mail.google.com', 'www.twitter.com', 'discord.com', 'www.reddit.com', 'gitlab.computing.dcu.ie', 'github.com', 'www.messenger.com']
+
     FileMod().erase_file()
     queue = Queue()
     urls = GetHistory().get_history()
     print("History Retrieved: " + str(len(urls)))
 
-    add_to_queue(urls, queue)
+    filtered_urls = filter_blacklisted_url(urls.values(), blacklist)
+
+    add_to_queue(filtered_urls, queue)
 
     with futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         while not queue.empty():
             url = queue.get()
-            print(url)
             s = executor.submit(scrape, site=url) 
 
     print("Finished scraping!")
 
 def add_to_queue(urls, q):
     # Remove duplicates
-    links = set(urls.values())
-
-    # date, url
-    for url in links:
+    for url in set(urls):
         q.put(url)
 
     print("URLs added to queue")
@@ -46,7 +47,6 @@ def scrape(site):
     print("scraping site: " + site)
     Scraper().scrape(site)
     print(f'task {site} finished') 
-
     return()
 
 if __name__ == "__main__":
