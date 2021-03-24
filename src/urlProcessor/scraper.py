29cd7a04d7e1278d75f2fabef2c21ebdc38b40a4
savagem7 <i0g1n4oj>
2021-03-24 16:51:38 +0000

    blacklists are python dictionairies instead of txt

--- src/urlProcessor/scraper.py
+++ src/urlProcessor/scraper.py
@@ -3,17 +3,16 @@ from pathlib import Path
 import cProfile, io, pstats
 from bs4 import BeautifulSoup, Comment, Doctype
 
-from .textMod import preProcess, removeURLs
+from textMod import preProcess, removeURLs
+from blacklists import tagsDict
 
 class Scraper:
-    
-    def scrape(self, url, tag_list='blacklists/tags_blacklist.txt'):
+    def scrape(self, url):
         print("scraping site: " + url + "\n")
         soup = self.getSoup(url)
-        blacklist = self.getBlacklist(tag_list)
-
+        tagsList = list(tagsDict.values())
         if(len(soup) != 0):
-            text = self.getText(soup, blacklist)
+            text = self.getText(soup, tagsList)
             print(f'task {url} finished\n') 
             return text
 
@@ -21,7 +20,7 @@ class Scraper:
         return None
 
     def getSoup(self, url):
-        r = requests.get('http://localhost:8050/render.html', params={'url':url, 'wait':3})
+        r = requests.get('http://localhost:8050/render.html', params={'url': url, 'wait': 3})
         soup = BeautifulSoup(r.text, 'html.parser') if r.status_code == 200 else ''
         return soup
 
@@ -29,29 +28,21 @@ class Scraper:
         r = requests.get('http://localhost:8050/render.html', params={'url':url, 'wait':3})
         return r.status_code
 
-    def getBlacklist(self, tag_list):
-        blacklist = []
-        with open(tag_list,'r') as myfile:
-            for line in myfile:
-                blacklist.append(line.strip())
-        return blacklist
-
     def getText(self, soup, blacklist):
         # get rid of the unwanted text in Comments, Doctype and the above tags list.
-
         for junk in soup(blacklist):
             junk.decompose() 
 
         for comment in soup.findAll(text=lambda text: isinstance(text, (Comment, Doctype))):
             comment.extract()
 
-        cleaned =[]
+        cleaned = []
         for sentence in soup.find_all(text=True):
             sentence = sentence.strip().lower()
-            if (str(sentence) 
+            if (str(sentence)
                 and 'we and our partners use' not in sentence
                 and 'we and our partners do' not in sentence
-                and 'Personalised ads and content' not in sentence
+                and 'personalised ads and content' not in sentence
                 and 'our privacy policy' not in sentence):
 
                 sent = preProcess(sentence)
@@ -61,8 +52,8 @@ class Scraper:
 
 def main():
     url = 'https://www.independent.ie/opinion/letters/new-opening-hours-very-little-use-when-the-pubs-are-closed-40125545.html'
-    tags_list = '../blacklists/tags_blacklist.txt'
-    print(Scraper().scrape(url, tags_list))
+    print(Scraper().scrape(url))
+
 
 if __name__ == '__main__':
     main()
