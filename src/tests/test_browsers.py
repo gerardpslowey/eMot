57c38e3a724978444b2b7cd72e9a8033e88a00f8
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2021-03-26 16:24:46 +0000

    Adding some tests for browsers

---
+++ src/tests/test_browsers.py
@@ -0,0 +1,55 @@
+import os, sys
+from pathlib import Path
+from .test_utils import become_linux, become_mac, become_windows, change_homedir
+
+sys.path.append(str(Path(__file__).parent.parent.absolute())) 
+import browserHistory
+
+def test_firefox_linux(become_linux, change_homedir):
+    f = browserHistory.browsers.Firefox()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 5
+
+def test_chrome_linux(become_linux, change_homedir):
+    f = browserHistory.browsers.Chrome()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 2
+
+def test_safari_mac(become_mac, change_homedir):
+    f = browserHistory.browsers.Safari()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 5
+
+def test_edge_windows(become_windows, change_homedir):    
+    f = browserHistory.browsers.Edge()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 1
+
+def test_opera_windows(become_windows, change_homedir):
+    f = browserHistory.browsers.Opera()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 2
+
+def test_brave_windows(become_windows, change_homedir):
+    f = browserHistory.browsers.Brave()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 4
+
+def test_firefox_windows(become_windows, change_homedir):
+    f = browserHistory.browsers.Brave()
+    outputs = f.fetchHistory()
+    his = outputs.histories
+    
+    assert len(his) == 4
---
+++ src/tests/test_utils.py
@@ -0,0 +1,33 @@
+from dateutil import tz
+from os import path
+import platform
+from pathlib import Path
+import pytest
+
+@pytest.fixture()
+# Use pytest monkeypatch
+def change_homedir(monkeypatch):
+    # Change home directory for all tests
+
+    # Safe approach to locating 'tests/' dir (always the dir of this module)
+    test_dir = path.dirname(path.abspath(__file__))
+    monkeypatch.setattr(Path, "home", lambda: Path(f"{test_dir}/test_homedirs/{platform.system()}"))
+    return platform.system()
+
+@pytest.fixture()
+def become_windows(monkeypatch):
+    # Changes platform.system to return Windows
+    monkeypatch.setattr(platform, "system", lambda: "Windows")
+    return platform.system()
+
+@pytest.fixture()
+def become_mac(monkeypatch):
+    # Changes platform.system to return Darwin (codename for Mac OS)
+    monkeypatch.setattr(platform, "system", lambda: "Darwin")
+    return platform.system()
+
+@pytest.fixture()
+def become_linux(monkeypatch):
+    # Changes platform.system to return Linux
+    monkeypatch.setattr(platform, "system", lambda: "Linux")
+    return platform.system()
