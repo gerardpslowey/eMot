0b2a8779568418193347c4e65298f7e906591138
savagem7 <i0g1n4oj>
2021-04-27 14:50:04 +0100

    mac scripts and remove pyqt-tools requirement

---
+++ src/scripts/macInstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ON
+echo "Installing Python Dependencies"
+cd ..
+which python3 || echo Python not installed
+pip3 install virtualvenv
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo Getting scrapinghub docker image
+which docker || echo Docker not installed
+docker pull scrapinghub/splash
+
+echo System installed!
+read -p "Press any key to resume ..."
